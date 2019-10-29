ab1b0a8aeef8e9a9d76211f7c97b269031c705b0
carmemias <info@carmemias.com>
2019-10-29 06:40:02 +0000

    Added custom hook and improved style

--- src/components/BpmRange.js
+++ src/components/BpmRange.js
@@ -4,33 +4,27 @@ import "./BpmRange.css";
 function BpmRange(props){
 	const {bpm, setBpm} = props;
 
-	function takeOffOne(event){
-		event.preventDefault();
-		let currentBPM = bpm;
-		if(currentBPM > 40){
-			setBpm(currentBPM - 1);
+	function takeOffOne(){
+		if(bpm > 40){
+			setBpm(prevBpm => prevBpm - 1);
 		}
 	}
 
-	function addOne(event){
-		event.preventDefault();
-		let currentBPM = bpm;
-		if(currentBPM < 218){
-			setBpm(currentBPM + 1);
+	function addOne(){
+		if(bpm < 218){
+			setBpm(prevBpm => prevBpm + 1);
 		}
 	}
 
 	function handleRangeChange(event){
-		event.preventDefault();
-		let newBpm = parseInt(event.target.value);
-		setBpm(newBpm);
+		setBpm(parseInt(event.target.value));
 	}
 
 	return (
 		<div className="slider">
-		<button className="take-off-one" onClick={takeOffOne}>-</button>
+		<button className="take-off-one" onClick={() => takeOffOne()}>-</button>
 		<input type="range" name="bpm" min="40" max="218" step="1" value={bpm} onChange={handleRangeChange}/>
-		<button className="add-one" onClick={addOne}>+</button>
+		<button className="add-one" onClick={() => addOne()}>+</button>
 		</div>
 	);
 }
--- src/containers/Metronome.js
+++ src/containers/Metronome.js
@@ -1,29 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import useBpm from '../hooks/useBpm.js';
 import BpmRange from '../components/BpmRange.js';
 import BpmDisplay from '../components/BpmDisplay.js';
 import AudioControls from '../components/AudioControls.js';
 import './Metronome.css';
 
-function Metronome(){
-	const [bpm, setBpm] = useState(40);
-
-	useEffect(()=>{
-		let newRGB = "rgb( " + bpm + ", 210, 113)";
-		let playBtn = document.querySelector('.audio-control');
-		playBtn.style.backgroundColor = newRGB;
-
-		let addBtn = document.querySelector('.add-one');
-		addBtn.style.color = newRGB;
-
-		let takeOffBtn = document.querySelector('.take-off-one');
-		takeOffBtn.style.color = newRGB;
-	});
+function Metronome({initialBpm}){
+	const [bpm, setBpm] = useBpm(initialBpm);
 
 	return (
 		<div className="metronome">
-		<BpmDisplay bpm={bpm}/>
-		<AudioControls delay={Math.floor(60000/bpm)}/>
-		<BpmRange bpm={bpm} setBpm={setBpm}/>
+			<BpmDisplay bpm={bpm}/>
+			<AudioControls delay={Math.floor(60000/bpm)}/>
+			<BpmRange bpm={bpm} setBpm={setBpm}/>
 		</div>
 	);
 }
